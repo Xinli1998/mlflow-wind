a3d8d68135e40d862825407e5ecf5ced79d0a5b6
Xinli1998 <107384855+Xinli1998@users.noreply.github.com>
2022-11-29 01:00:42 +0800

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -42,8 +42,11 @@ from sklearn.neural_network import MLPRegressor
 # Start a run
 # TODO: Set a descriptive name. This is optional, but makes it easier to keep track of your runs.
 def workflow(alpha, max_iter, degree, number_of_splits):
+    experiment = mlflow.get_experiment_by_name("xinl")
+    client = mlflow.tracking.MlflowClient()
+    run = client.create_run(experiment.experiment_id)
 
-#     with mlflow.start_run(run_name='mlflow-wind'):
+    with mlflow.start_run(run_name='mlflow-wind'):
         # TODO: Insert path to dataset
         df = pd.read_json("dataset.json", orient="split")
 
