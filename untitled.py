6be08db9d6fe88d3569d2405700c37e4c019d48b
Xinli1998 <107384855+Xinli1998@users.noreply.github.com>
2022-11-28 23:14:22 +0800

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -5,7 +5,7 @@ import sys
 
 ## NOTE: Optionally, you can use the public tracking server.  Do not use it for data you cannot afford to lose. See note in assignment text. If you leave this line as a comment, mlflow will save the runs to your local filesystem.
 
-mlflow.set_tracking_uri("http://localhost:5000/")
+# mlflow.set_tracking_uri("http://localhost:5000/")
 
 # TODO: Set the experiment name
 mlflow.set_experiment("xinl")
